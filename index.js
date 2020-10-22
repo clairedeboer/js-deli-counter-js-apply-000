c161b4614705e517f093f29ac0d3cffeb8ca7d68
Claire DeBoer <claire.deboer@gmail.com>
2020-10-22 15:28:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+let newNumber = 0; 
+const takeANumber = (katzDeliLine) => {
+  newNumber++; 
+  katzDeliLine.push(newNumber); 
+  return `Welcome, you are ${newNumber}.`
+}
+
+
+
+
+
+
 const takeANumber = (katzDeliLine, newName) => {
   katzDeliLine.push(newName); 
   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
@@ -5,18 +17,20 @@ const takeANumber = (katzDeliLine, newName) => {
 
 const nowServing  = (katzDeliLine) => {
   if (katzDeliLine.length >= 1) {
-    const next = katzDeliLine.shift(); 
-    return `Currently serving ${next}.`
+    const nextInLine = katzDeliLine.shift(); //shift method removes first element of array (modifies) and returns
+    return `Currently serving ${nextInLine}.`
   } else {
-    return 'There is nobody waiting to be served!'
+    return 'There is nobody waiting to be served!';
   }
 }
 
 const currentLine = (katzDeliLine) => {
   if (katzDeliLine.length >= 1) {
-  const formatted = (name, i) => i+1 + ". " + name; 
-  return `The line is currently: ${katzDeliLine.map(formatted).join(", ")}`
+    const formatFunc = (name, i) => i + 1 + ". " + name; 
+    return `The line is currently: ${katzDeliLine.map(formatFunc).join(", ")}`; //join puts array in a string
   } else {
-    return 'The line is currently empty.'
+    return 'The line is currently empty.'; 
   }
 }
+//map returns edited array
+//join for arrays, will not add ", " to last element
